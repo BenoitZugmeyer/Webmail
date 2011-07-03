dfa896cf5c1acaf1c40783b0d9ca6af56a788196
Benoît Zugmeyer <bzugmeyer@gmail.com>
2011-07-04 00:16:09 +0200

    Update database

--- sync.js
+++ sync.js
@@ -24,21 +24,90 @@ function error (e) {
 
 function fetch (imap, uids, options, callback) {
   var fetcher = imap.fetch(uids, options)
-    , messages = [];
+    , messages = []
+    , group = options && options.group !== undefined || 100;
+
   fetcher.on('message', function (msg) {
     msg.on('end', function () {
       messages.push(msg);
+      if(messages.length === group) {
+        callback(null, messages, true);
+        messages = [];
+      }
     })
   });
   fetcher.on('end', function () {
-    callback(null, messages);
+    callback(null, messages, false);
   });
+}
+
+function storeMessages (db, mailbox, messages, callback) {
+  log('Storing ' + messages.length + ' messages');
+  messages = messages.map(function(m) { return {
+    mailbox: dbId(db, mailbox),
+    id: m.id,
+    subject: m.headers.subject,
+    flags: m.flags
+  }; });
+  db.collection('mails').insertAll(messages, callback);
+}
+
+function arrayEqual(a, b) {
+  if(a === b) { return true; }
+  if(!a || !b || a.length !== b.length) { return false; }
+
+  var i, l;
+  for(i = 0, l = a.length; i < l; i += 1) {
+    if(a[i] !== b[i]) { return false; }
+  }
+  return true;
+}
+
+function updateMessages (db, mailbox, messages, callback) {
+  log('Updating ' + messages.length + ' messages');
+  Step(
+    function() {
+      var step = this.parallel(), self = this;
+      db.collection('mails').find({ mailbox: dbId(db, mailbox) },
+                                  { id: true, flags: true, sort: 'id' })
+        .each(function(e, dbMessage) {
+          if(e || !dbMessage) { return step(e); }
+          var imapMessage = messages[0] || { id: Number.MAX_VALUE };
+          if(imapMessage.id === dbMessage.id) {
+            if(!arrayEqual(imapMessage.flags, dbMessage.flags)) {
+              log('Update flags of ' + dbMessage.id);
+              db.collection('mails').updateById(
+                dbMessage._id,
+                { $set: { flags: imapMessage.flags } },
+                self.parallel()
+              );
+            }
+            messages.shift();
+          }
+          else if(imapMessage.id > dbMessage.id) {
+            log('Delete message ' + dbMessage.id);
+            db.collection('mails').removeById(dbMessage._id, self.parallel());
+          }
+          else {
+            log('ERROR');
+            messages.shift();
+          }
+        }
+      );
+    },
+    function (e) {
+      callback(e, messages.map(function(m) { return m.id; }));
+    }
+  );
 }
 
 function log (message) {
   console.log('//', message);
 }
 
+function dbId (db, object) {
+  return db.db.bson_serializer.ObjectID.createFromHexString(object._id)
+}
 
 // cf: https://github.com/rgrove/larch/blob/master/lib/larch.rb
 //     https://github.com/rgrove/larch/blob/master/lib/larch/imap/mailbox.rb
@@ -46,13 +115,21 @@ function sync (box_name) {
   var imapMailbox
     , dbMailbox
     , imap
+    , make = function (what, step, next) {
+        return function (e, messages, more) {
+          if(e) { next(e); }
+          what(db, dbMailbox, messages, more ? step.parallel() : next);
+        };
+      }
     , db = mongo.db('localhost/mail?auto_reconnect');
   Step(
     function () {
       log('Connection');
 
       Step(
-        function () { getCredentials(this); },
+        function () {
+          getCredentials(this);
+        },
         function (e, username, password) {
           error(e);
           imap = new ImapConnection({
@@ -62,88 +139,104 @@ function sync (box_name) {
             port: 993,
             secure: true,
             debug: function(s) {
-              //  console.log(s.trim().replace('<<RECEIVED>>', '-->').replace('<<SENT>>', '<--'));
+    //          console.log(s.trim().replace('<<RECEIVED>>', '-->').replace('<<SENT>>', '<--'));
             }
           });
           imap.connect(this);
         },
-        function (e) { error(e); imap.openBox(box_name, true, this); },
+        function (e) {
+          error(e);
+          imap.openBox(box_name, true, this);
+        },
         this.parallel()
       );
 
       db.collection('mailboxes').findOne({name: box_name}, this.parallel());
     },
     function (e, imapMailbox_, dbMailbox_) {
+      error(e);
+      log('Retrieve infos');
+
       imapMailbox = imapMailbox_;
       dbMailbox = dbMailbox_;
-      log('Retrieve infos');
-      error(e);
-      var add = this.parallel()
+      var self = this
         , update = this.parallel();
-      if(dbMailbox && dbMailbox.uidnext && dbMailbox.validity === imapMailbox.validity) {
-        if(dbMailbox.uidnext === imapMailbox.uidnext) {
-          add(); // nothing to add
-        }
-        else {
+
+      if(dbMailbox && dbMailbox.uidnext &&
+         dbMailbox.validity === imapMailbox.validity
+        ) {
+        if(dbMailbox.uidnext !== imapMailbox.uidnext) {
           log('Fetch mails to add');
           fetch(imap,
                 dbMailbox.uidnext + ':' + imapMailbox.uidnext,
                 {},
-                add);
+                make(storeMessages, this, this.parallel())
+               );
         }
         log('Fetch mails to update');
         fetch(imap,
               '1:' + dbMailbox.uidnext,
-              { request: { struct: false, headers: false } },
-              update);
+              { group: 0, request: { struct: false, headers: false } },
+              make(updateMessages, this, update)
+             );
       }
       else {
         update(); // nothing to update
         if(dbMailbox) {
           log('Remove all messages');
           db.collection('mails').remove(
-            {mailbox: db.db.bson_serializer.ObjectID.createFromHexString(dbMailbox._id)},
-            this.parallel());
-            db.collection('mailboxes').updateById(dbMailbox._id, { $set: { validity: imapMailbox.validity } }, this.parallel());
+            {mailbox: dbId(db, dbMailbox)},
+            this.parallel()
+          );
+          log('Updating mailbox validity uid');
+          db.collection('mailboxes').updateById(
+            dbMailbox._id,
+            { $set: { validity: imapMailbox.validity } },
+            this.parallel()
+          );
         }
         else {
-          db.collection('mailboxes').insert({name: box_name, validity: imapMailbox.validity}, this.parallel());
+          log('Create the mailbox');
+          dbMailbox = db.collection('mailboxes').insert(
+            {name: box_name, validity: imapMailbox.validity},
+            this.parallel()
+          );
         }
         log('Download all messages');
         fetch(imap,
               '1:' + imapMailbox.uidnext,
               {},
-              add);
+              make(storeMessages, this, this.parallel())
+             );
       }
     },
 
-    function (e, add, update) {
-      error(e);
-      if(add && add.length) {
-        log('Add new mails to database');
-        add.forEach(function (mail) {
-          console.log(mail.headers.subject && mail.headers.subject[0]);
-        });
-        //      console.log(add);
-        db.collection('mailboxes').updateById(dbMailbox._id, { $set: { uidnext: imapMailbox.uidnext } });
-      }
-      if(update && update.length) {
-        log('Update mails of database');
-        //      console.log(update);
-      }
-      this();
+    function (e, toDownload) {
+      if(e || !toDownload || !toDownload.length) { return this(e); }
+      log('Download missing emails (should not happen)');
+      fetch(imap, toDownload, {}, make(storeMessages, this, this.parallel()));
     },
 
     function (e) {
-      log('Close connections');
       error(e);
+      log('Done adding / updating, updating db uidnext');
+      db.collection('mailboxes').updateById(
+        dbMailbox._id,
+        { $set: { uidnext: imapMailbox.uidnext } },
+        this
+      );
+    },
+
+    function (e) {
+      error(e);
+      log('Close connections');
       imap.logout(this);
       db.close();
     },
 
     function (e) {
-      log('End');
       error(e);
+      log('The End');
     }
   );
 }
